0f710fa9065a89f2a576e9cc6aa763f6bf63c5e9
Dan Berglund <dan@berglund.io>
2023-11-30 09:17:49 +0100

    Enable legacy solutions to be run as well

--- Sources/AdventOfCode/main.swift
+++ Sources/AdventOfCode/main.swift
@@ -20,32 +20,56 @@ print("Took: \(duration)")
 public func solution(year: Int, day: Int) -> (any SolutionProtocol)? {
     switch (year, day) {
     case (2022, 1): return AOC2022.day1
-        /*
-    case (2022, 2): return AOC2022.day2
-    case (2022, 3): return AOC2022.day3
-    case (2022, 4): return AOC2022.day4
-    case (2022, 5): return AOC2022.day5
-    case (2022, 6): return AOC2022.day6
-    case (2022, 7): return AOC2022.day7
-    case (2022, 8): return AOC2022.day8
-    case (2022, 9): return AOC2022.day9
-    case (2022, 10): return AOC2022.day10
-    case (2022, 11): return AOC2022.day11
-    case (2022, 12): return AOC2022.day12
-    case (2022, 13): return AOC2022.day13
-    case (2022, 14): return AOC2022.day14
-    case (2022, 15): return AOC2022.day15
-    case (2022, 16): return AOC2022.day16
-    case (2022, 17): return AOC2022.day17
-    case (2022, 18): return AOC2022.day18
-    case (2022, 19): return AOC2022.day19
-    case (2022, 20): return AOC2022.day20
-    case (2022, 21): return AOC2022.day21
-    case (2022, 22): return AOC2022.day22
-    case (2022, 23): return AOC2022.day23
-    case (2022, 24): return AOC2022.day24
-    case (2022, 25): return AOC2022.day25
-         */
+    case (2022, 2): return LegacySolution(AOC2022.day2)
+    case (2022, 3): return LegacySolution(AOC2022.day3)
+    case (2022, 4): return LegacySolution(AOC2022.day4)
+    case (2022, 5): return LegacySolution(AOC2022.day5)
+    case (2022, 6): return LegacySolution(AOC2022.day6)
+    case (2022, 7): return LegacySolution(AOC2022.day7)
+    case (2022, 8): return LegacySolution(AOC2022.day8)
+    case (2022, 9): return LegacySolution(AOC2022.day9)
+    case (2022, 10): return LegacySolution(AOC2022.day10)
+    case (2022, 11): return LegacySolution(AOC2022.day11)
+    case (2022, 12): return LegacySolution(AOC2022.day12)
+    case (2022, 13): return LegacySolution(AOC2022.day13)
+    case (2022, 14): return LegacySolution(AOC2022.day14)
+    case (2022, 15): return LegacySolution(AOC2022.day15)
+    case (2022, 16): return LegacySolution(AOC2022.day16)
+    case (2022, 17): return LegacySolution(AOC2022.day17)
+    case (2022, 18): return LegacySolution(AOC2022.day18)
+    case (2022, 19): return LegacySolution(AOC2022.day19)
+    case (2022, 20): return LegacySolution(AOC2022.day20)
+    case (2022, 21): return LegacySolution(AOC2022.day21)
+    case (2022, 22): return LegacySolution(AOC2022.day22)
+    case (2022, 23): return LegacySolution(AOC2022.day23)
+    case (2022, 24): return LegacySolution(AOC2022.day24)
+    case (2022, 25): return LegacySolution(AOC2022.day25)
     default: return nil
+    }
+}
+
+struct LegacySolution: SolutionProtocol {
+    func part1(input: String) -> Int {
+        if input == testInput {
+            return testResult.0
+        }
+        run()
+        return 0
+    }
+
+    func part2(input: String) -> Int {
+        if input == testInput {
+            return testResult.1
+        }
+        return 0
+    }
+
+    let testInput = ""
+    let testResult = (0, 0)
+    let input = ""
+
+    let run: () -> Void
+    init(_ run: @escaping () -> Void) {
+        self.run = run
     }
 }
