34b1163a9447a2fec4024474a58b1e9479aa7c8b
Dan Berglund <dan@berglund.io>
2023-12-12 22:00:02 +0100

    Dynamically generate test-cases nicer

--- Tests/AdventOfCodeTests/TestRunner.swift
+++ Tests/AdventOfCodeTests/TestRunner.swift
@@ -12,45 +12,40 @@ extension SolutionProtocol {
     }
 }
 
-class TestRunner: XCTestCase {
-    func testToday() throws {
-        let year: Int = Calendar.current.component(.year, from: Date())
-        let day: Int = Calendar.current.component(.day, from: Date())
-        let solution = try XCTUnwrap(AdventOfCode.solution(year: year, day: day))
-        solution.testPart1()
-        solution.testPart2()
-    }
-
+class Year2022: XCTestCase {
     override class var defaultTestSuite: XCTestSuite {
-        let suite = XCTestSuite(forTestCaseClass: TestRunner.self)
-
-        let years = [2022, 2023]
-        let days = (1...25)
-        for year in years {
-            for day in days {
-                if let solution = AdventOfCode.solution(year: year, day: day) {
-                    let part1 = TestSolution(selector: #selector(TestSolution.runPart1))
-                    part1.solution = solution
-                    suite.addTest(part1)
-
-                    let part2 = TestSolution(selector: #selector(TestSolution.runPart2))
-                    part2.solution = solution
-                    suite.addTest(part2)
-                }
-            }
-        }
-        return suite
+        createTestSuite(testCase: Year2022.self, year: 2022)
     }
 }
 
-class TestSolution: XCTestCase {
-    var solution: (any SolutionProtocol)!
-
-    func runPart1() {
-        solution.testPart1()
+class Year2023: XCTestCase {
+    override class var defaultTestSuite: XCTestSuite {
+        createTestSuite(testCase: Year2023.self, year: 2023)
     }
+}
 
-    func runPart2() {
-        solution.testPart2()
+func createTestSuite(testCase: XCTestCase.Type, year: Int) -> XCTestSuite {
+    let suite = XCTestSuite(forTestCaseClass: testCase)
+    let days = (1...25)
+    //        for (year, clsss) in years {
+    for day in days {
+        if let solution = AdventOfCode.solution(year: year, day: day) {
+            suite.addTest(addMethod(testCase: testCase, name: "day_\(day)_part1") {
+                solution.testPart1()
+            })
+            suite.addTest(addMethod(testCase: testCase, name: "day_\(day)_part2") {
+                solution.testPart2()
+            })
+        }
     }
+    return suite
+}
+
+private func addMethod(testCase: XCTestCase.Type, name: String, block: @escaping () -> Void) -> XCTestCase {
+    let cls: AnyClass = testCase
+    let sel = Selector((name))
+    let block: @convention(block) () -> Void = block
+    let imp = imp_implementationWithBlock(block)
+    class_addMethod(cls, sel, imp, "d@:")
+    return testCase.init(selector: sel)
 }
