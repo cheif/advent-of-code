75ea0154c0e03506e466512aa6753dff2567d21c
Dan Berglund <dan@berglund.io>
2023-12-12 22:09:33 +0100

    Nicer formatting, bring back today-test

--- Tests/AdventOfCodeTests/TestRunner.swift
+++ Tests/AdventOfCodeTests/TestRunner.swift
@@ -12,6 +12,22 @@ extension SolutionProtocol {
     }
 }
 
+class Today: XCTestCase {
+    func testPart1() throws {
+        let year: Int = Calendar.current.component(.year, from: Date())
+        let day: Int = Calendar.current.component(.day, from: Date())
+        let solution = try XCTUnwrap(AdventOfCode.solution(year: year, day: day))
+        solution.testPart1()
+    }
+
+    func testPart2() throws {
+        let year: Int = Calendar.current.component(.year, from: Date())
+        let day: Int = Calendar.current.component(.day, from: Date())
+        let solution = try XCTUnwrap(AdventOfCode.solution(year: year, day: day))
+        solution.testPart2()
+    }
+}
+
 class Year2022: XCTestCase {
     override class var defaultTestSuite: XCTestSuite {
         createTestSuite(testCase: Year2022.self, year: 2022)
@@ -27,13 +43,16 @@ class Year2023: XCTestCase {
 func createTestSuite(testCase: XCTestCase.Type, year: Int) -> XCTestSuite {
     let suite = XCTestSuite(forTestCaseClass: testCase)
     let days = (1...25)
-    //        for (year, clsss) in years {
+
+    let formatter = NumberFormatter()
+    formatter.minimumIntegerDigits = 2
+
     for day in days {
         if let solution = AdventOfCode.solution(year: year, day: day) {
-            suite.addTest(addMethod(testCase: testCase, name: "day_\(day)_part1") {
+            suite.addTest(addMethod(testCase: testCase, name: "testDay\(formatter.string(from: day as NSNumber)!)_part1") {
                 solution.testPart1()
             })
-            suite.addTest(addMethod(testCase: testCase, name: "day_\(day)_part2") {
+            suite.addTest(addMethod(testCase: testCase, name: "testDay\(formatter.string(from: day as NSNumber)!)_part2") {
                 solution.testPart2()
             })
         }
