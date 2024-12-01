232b0691e6d5ad80ed2102acc9982fd5af4c7db3
Dan Berglund <dan@berglund.io>
2024-12-01 06:11:23 +0100

    Day 1

--- Tests/AOC2024Tests/AOC2024Tests.swift
+++ Tests/AOC2024Tests/AOC2024Tests.swift
@@ -5,11 +5,25 @@ import Testing
 struct Day1 {
     @Test
     func p1() {
-        #expect(day1.part1(input: "") == 0)
+        #expect(day1.part1(input: """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""") == 11)
     }
 
     @Test
     func p2() {
-        #expect(day1.part2(input: "") == 0)
+        #expect(day1.part2(input: """
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+""") == 31)
     }
 }
