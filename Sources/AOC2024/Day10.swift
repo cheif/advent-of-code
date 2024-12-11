01dd29be346773f3470351a4a5f5b57c1d516589
Dan Berglund <dan@berglund.io>
2024-12-11 06:27:07 +0100

    Remove unused code

--- Sources/AOC2024/Day10.swift
+++ Sources/AOC2024/Day10.swift
@@ -13,7 +13,6 @@ private func part2(input: String) -> Int {
     let grid = Grid<Int>(data: Grid(string: input).data.map { Grid<Int>.Point(x: $0.x, y: $0.y, val: Int(String($0.val))!) })
     let heads = grid.data.filter { $0.val == 0 }.sorted { $0.y < $1.y }
     let visited = heads.flatMap { point in paths(grid: grid, start: point) }
-    let unique = Set(visited.map { $0.prefix(1) + $0.suffix(1) })
     return visited.count
 }
 
